130c64e2b6bf1d9a1705fbad1cf7cb6719f34b7c
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2024-02-05 21:01:03 +0900

    Update news_preview.vue

--- sample_code/front-end/nuxtjs/news_preview.vue
+++ sample_code/front-end/nuxtjs/news_preview.vue
@@ -5,16 +5,20 @@
   </div>
 </template>
 
-<script>
-export default {
-  async asyncData({ $axios, route }) {
-    return {
-      response: await $axios.$get(`/rcms-api/6/news/preview`, {
-        params: {
-          preview_token: route.query.preview_token,
-        },
-      }),
-    };
-  },
-};
+<script setup>
+import { useFetch } from 'nuxt3';
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+
+const response = ref(null);
+const route = useRoute();
+
+useFetch(async ({ $axios }) => {
+  response.value = await $axios.$get(`/rcms-api/6/news/preview`, {
+    params: {
+      preview_token: route.value.query.preview_token,
+    },
+  })
+})
+
 </script>
