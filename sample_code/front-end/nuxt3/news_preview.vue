efc62bbc5bf9cc2937f391440fec4cad2b8d1e08
HisashiHasebe <hhasebe@diverta.co.jp>
2024-04-09 10:12:08 +0800

    fix:endpoint url

--- sample_code/front-end/nuxt3/news_preview.vue
+++ sample_code/front-end/nuxt3/news_preview.vue
@@ -14,7 +14,7 @@ const route = useRoute();
 const preview_token = route.query.preview_token;
 
 const { data: response } = await useFetch(
-  `${config.public.apiBase}/rcms-api/6/preview`,
+  `${config.public.apiBase}/rcms-api/6/news/preview`,
   {
     credentials: 'include',
     params: {
